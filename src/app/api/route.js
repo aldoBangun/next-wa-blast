597629f30cd140b732e036126995dd9a107f9395
Alfredo Putra Djaru Bangun <alfredo@mk-indonesia.com>
2023-05-23 15:44:22 +0700

    separate processRequest from api route

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server'
-import * as config from '@/utils/apiRoutes'
+import processRequest from '@/utils/processRequest'
 import axios from 'axios'
 
 axios.defaults.baseURL = `http://${process.env.API_HOST}:${process.env.API_PORT}`
@@ -12,43 +11,8 @@ export async function GET(request) {
 
 export async function POST(request) {
   const reqBody = await request.json()
+  
   return await processRequest(request, async (action, subAction) => {
     return await axios.post(`/${action}/${subAction}`, { reqBody })
-  })
-}
-
-/* Utils */
-
-function getRoute(request) {
-  const { searchParams } = new URL(request.url)
-  const path = searchParams.get('path')
-  const destRoute = searchParams.get('dest')
-
-  return new Promise((resolve, reject) => {
-    const configPath = config[path]
-    if (!configPath) return reject(new Error(`Path "${path}" does not exists!`))
-
-    const configDestRoute = configPath[destRoute]
-    if (!configDestRoute) return reject(new Error(`Route "${destRoute}" does not exists!`))
-
-    const { action, subAction } = configDestRoute
-    resolve({ action, subAction })
-  })
-}
-
-function processRequest(request, callback) {
-  return new Promise(async (resolve) => {
-    try {
-      const { action, subAction } = await getRoute(request)
-      const res = await callback(action, subAction)
-
-      resolve(NextResponse.json(res.data))
-    } catch(err) {
-      resolve(NextResponse.json({
-        code: -1,
-        message: err?.data?.message || err?.message || 'Something error on backend',
-        content: null
-      }))
-    }
   })
 }
